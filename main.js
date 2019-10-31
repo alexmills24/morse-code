c06aba8486f941d31a6936523a23ab956585e588
alexmills24 <55689935+alexmills24@users.noreply.github.com>
2019-10-31 13:48:20 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const morseAlphabet = {'a':'. . -', 'b':'- . .', 'c':'- . - .', 'd':'- . .', 'e':'.', 'f':'. . - .', 'g':'- - .', 'h':'. . . .', 'i':'. .', 'j':'. - - -', 'k':'- . -', 'l':'. - . .', 'm':'- -', 'n':'- .', 'o':'- - -', 'p':'. - - .', 'q':'- - . -', 'r':'. - .', 's':'. . .', 't':'-', 'u':'. . -', 'v':'. . . -', 'w':'. - -', 'x':'- . . -', 'y':'- . - -', 'z':'- - . .'};
+
+const enteredText = document.getElementById('entered-text');
+const translatedText = document.getElementById('translated');
+
+function convertText(){
+    let makeLowerCase = enteredText.value;
+    makeLowerCase = makeLowerCase.toLowerCase();
+    makeLowerCase = makeLowerCase.split("");
+    for (index = 0; index < makeLowerCase.length; index++){
+        makeLowerCase[index] = morseAlphabet[makeLowerCase[index]]
+    }
+    makeLowerCase = makeLowerCase.join(" ")
+    translatedText.innerHTML = makeLowerCase;
+};
+
+
+const reverseAlphabet = {'. . -':'a', '- . .':'b', '- . - .':'c', '- . .':'d', '.':'e', '. . - .':'f', '- - .':'g', '. . . .':'h', '. .':'i', '. - - -':'j', '- . -':'k', '. - . .':'l', '- -':'m', '- .':'n', '- - -':'o', '. - - .':'p', '- - . -':'q', '. - .':'r', '. . .':'s', '-':'t', '. . -':'u', '. . . -':'v', '. - -':'w', '- . . -':'x', '- . - -':'y', '- - . .':'z'};
+const enteredMorse = document.getElementById('entered-morse');
+const translatedMorse = document.getElementById('translated-morse');
+
+function convertMorse(){
+    let morseCode = enteredMorse.value;
+    morseCode = morseCode.split("");
+    for (index = 0; index < morseCode.length; index++){
+        morseCode[index] = reverseAlphabet[morseCode[index]]
+    };
+    morseCode.join("")
+    translatedMorse.innerHTML = morseCode;
+}
